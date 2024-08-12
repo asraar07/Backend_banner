44827876b36396d643191d92b65cc6adcd681f9f
asraar07 <130930821+asraar07@users.noreply.github.com>
2024-08-12 17:45:21 +0530

    Update server.js000001

--- server.js
+++ server.js
@@ -16,8 +16,8 @@ res.send("Server is running")
 
 app.use('/api', bannerRoutes);
 // Start the server
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+
+app.listen(4000, () => {
+  console.log("Server running on port 4000");
 
 });
