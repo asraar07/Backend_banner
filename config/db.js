5297bd50250fd0d860936e2ee75aa094efc18c69
asraar07 <130930821+asraar07@users.noreply.github.com>
2024-08-12 17:08:07 +0530

    Update db.js0001

--- config/db.js
+++ config/db.js
@@ -9,6 +9,7 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
+  port: process.env.PORT,
   ssl: {
     rejectUnauthorized: process.env.DB_SSL_MODE === 'REQUIRED'
   }
